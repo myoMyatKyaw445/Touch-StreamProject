898aa3064a8f9579427962579bb9182057527aa0
MyoMyatKyaw <myomyatkyaw667@gmail.com>
2026-09-04 11:03:16 +0630

    Add MStream PWA manifest and icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header className="app-header">
       <div className="header-left">
         <button className="menu-btn">☰</button>
-        <h1 className="app-title">FMP Live</h1>
+        <h1 className="app-title">M Stream</h1>
       </div>
       <div className="header-right">
         <button className="icon-btn">⭐</button>
